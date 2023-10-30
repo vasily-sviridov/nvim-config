9657ae0086298bd2b9a8a8fdffa4a4c36c17219a
Vasily Sviridov <vasily-sviridov@adev-team.ru>
2023-10-30 21:48:35 +0300

    add keymap for toggle focus in treeview

--- lua/tree_keymaps.lua
+++ lua/tree_keymaps.lua
@@ -1,7 +1,7 @@
 local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true, silent = true}
 
-map("n", "<c-n>", ":NvimTreeToggle<CR>", {noremap = true})
+map("n", "<c-n>", ":NvimTreeToggle<CR>", default_opts)
 map("i", "<c-n>", "<esc>:NvimTreeToggle<CR>", default_opts)
-
-
+map("n", "<c-a>", ":wincmd p<CR>", default_opts)
+map("i", "<c-a>", "<esc>:wincmd p<CR>", default_opts)
