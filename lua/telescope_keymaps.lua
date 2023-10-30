7049b2fc2680e6f29f3df5b463aa03e84d9713ee
Vasily Sviridov <cto@isaev-group.ru>
2023-10-30 22:42:23 +0300

    update

--- lua/telescope_keymaps.lua
+++ lua/telescope_keymaps.lua
@@ -1,5 +1,5 @@
 local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true, silent = true}
 
-map("n", "<C-f>", ":Telescope find_files<CR>", {silent = true})
-map("n", "<C-g>", ":Telescope live_grep<CR>", default_opts)
+map("n", "<A-f>", ":Telescope find_files<CR>", {noremap = false, silent = true})
+map("n", "<A-g>", ":Telescope live_grep<CR>", default_opts)
